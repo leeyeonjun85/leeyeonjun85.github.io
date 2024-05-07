35bdc249fabf8bcee6003d67541ff6f5d32b65b3
leeyeonjun85 <leeyeonjun85@gmail.com>
2024-05-07 09:05:43 +0900

    a

--- assets/source/Hotel_Booking/Hotel_Booking_Backend/Middleware/AuthorizeAttribute.cs
+++ assets/source/Hotel_Booking/Hotel_Booking_Backend/Middleware/AuthorizeAttribute.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Hotel_Booking_Backend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -10,12 +10,12 @@ public class AuthorizeAttribute : Attribute, IAuthorizationFilter
 {
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        var account = (LoginModel)context.HttpContext.Items["User"];
-        if (account == null)
-        {
-            // not logged in
-            context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
-        }
+        //var account = (LoginModel)context.HttpContext.Items["User"];
+        //if (account == null)
+        //{
+        //    // not logged in
+        //    context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        //}
     }
 }
 
